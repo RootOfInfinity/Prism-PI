9a80bc0a0b1189cc9cd364d5bb7d9dd898a2e430
William Campbell <numbersdev@protonmail.com>
2025-06-08 23:58:45 -0500

    added to the AST

--- src/lang/ast.rs
+++ src/lang/ast.rs
@@ -1,3 +1,26 @@
+use crate::lang::tokens::{Literal, Type};
+
 pub enum ExprAST {
     Var(String),
+}
+
+pub enum Statement {
+    Expr,
+    Decl,
+    Assign,
+    If,
+    While,
+}
+
+pub struct Assignment {
+    pub typ: Type,
+    pub ident: String,
+    ident_loc: Loc,
+    val: Literal,
+    val_loc: Loc,
+}
+
+pub struct Loc {
+    pub line: u32,
+    pub col: u32,
 }
--- src/lang/parser.rs
+++ src/lang/parser.rs
@@ -1,5 +1,8 @@
+use std::collections::VecDeque;
+
 use crate::lang::tokens::Token;
 
 pub struct ParsingMachine {
     cur_tok: Token,
+    tok_vec: VecDeque<Token>,
 }
