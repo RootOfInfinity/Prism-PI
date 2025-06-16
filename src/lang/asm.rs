be64e92d3708a45f1b429aff33d4835b6c205c9a
William Campbell <numbersdev@protonmail.com>
2025-06-16 17:11:57 -0500

    formatting and turning label asm into no labels

--- src/lang/asm.rs
+++ src/lang/asm.rs
@@ -1,5 +1,90 @@
+use std::collections::HashMap;
 
+use super::tokens::Type;
+
+pub struct AsmFile {
+    consts: (Vec<u8>, Vec<Type>),
+    pool: Vec<String>,
+    code: Vec<Instruction>,
+}
+impl AsmFile {
+    pub fn new(consts: (Vec<u8>, Vec<Type>), pool: Vec<String>, code: Vec<Instruction>) -> Self {
+        AsmFile { consts, pool, code }
+    }
+    pub fn extract_consts_and_pool(&self) -> (&(Vec<u8>, Vec<Type>), &Vec<String>) {
+        (&self.consts, &self.pool)
+    }
+    pub fn remove_labels(&self) -> Vec<NoLabelInst> {
+        let mut label_map: HashMap<&String, usize> = HashMap::new();
+        let mut new_code: Vec<&Instruction> = Vec::new();
+        let mut removed = 0;
+        for (i, inst) in self.code.iter().enumerate() {
+            let Instruction::Label(label) = inst else {
+                new_code.push(inst);
+                continue;
+            };
+            label_map.insert(label, i - removed);
+            removed += 1;
+        }
+        let mut no_labels: Vec<NoLabelInst> = Vec::new();
+        for inst in new_code {
+            no_labels.push(match inst {
+                Instruction::Jmp(x) => NoLabelInst::Jmp(*label_map.get(x).unwrap() as u32),
+                Instruction::Je(x) => NoLabelInst::Je(*label_map.get(x).unwrap() as u32),
+                Instruction::Jl(x) => NoLabelInst::Jl(*label_map.get(x).unwrap() as u32),
+                Instruction::Jle(x) => NoLabelInst::Jle(*label_map.get(x).unwrap() as u32),
+                Instruction::Jg(x) => NoLabelInst::Jg(*label_map.get(x).unwrap() as u32),
+                Instruction::Jge(x) => NoLabelInst::Jge(*label_map.get(x).unwrap() as u32),
+                Instruction::Call(x) => NoLabelInst::Call(*label_map.get(x).unwrap() as u32),
+                Instruction::Ret => NoLabelInst::Ret,
+                Instruction::Pop => NoLabelInst::Pop,
+                Instruction::Freet => NoLabelInst::Freet,
+                Instruction::Add => NoLabelInst::Add,
+                Instruction::Sub => NoLabelInst::Sub,
+                Instruction::Mul => NoLabelInst::Mul,
+                Instruction::Div => NoLabelInst::Div,
+                Instruction::And => NoLabelInst::And,
+                Instruction::Or => NoLabelInst::Or,
+                Instruction::Xor => NoLabelInst::Xor,
+                Instruction::Cmp => NoLabelInst::Cmp,
+                Instruction::Push(x, y) => NoLabelInst::Push(*x, *y),
+                Instruction::Mov(x, y, z) => NoLabelInst::Mov(*x, *y, *z),
+                Instruction::Alloc(x, y) => NoLabelInst::Alloc(*x, *y),
+                Instruction::Fun(x) => NoLabelInst::Fun(*x),
+                Instruction::Label(_) => unreachable!(),
+            });
+        }
+        no_labels
+    }
+}
+//bytetext asm has labels
 pub enum Instruction {
+    Ret,
+    Push(u8, u16),
+    Pop,
+    Mov(u16, u8, u16),
+    Alloc(u8, u16),
+    Freet,
+    Add,
+    Sub,
+    Mul,
+    Div,
+    And,
+    Or,
+    Xor,
+    Cmp,
+    Jmp(String),
+    Je(String),
+    Jl(String),
+    Jle(String),
+    Jg(String),
+    Jge(String),
+    Call(String),
+    Fun(u16),
+    Label(String),
+}
+
+pub enum NoLabelInst {
     Ret,
     Push(u8, u16),
     Pop,
@@ -21,5 +106,5 @@ pub enum Instruction {
     Jg(u32),
     Jge(u32),
     Call(u32),
-    Fun(u32),
+    Fun(u16),
 }
--- src/lang/assembler.rs
+++ src/lang/assembler.rs
@@ -1,106 +1,105 @@
-use super::asm::Instruction;
+use super::asm::{Instruction, NoLabelInst};
 //formatting will be fixed later
-pub fn to_le_bytes(inst: &Instruction) -> [u8; 6] {
-      let mut inst_slice: [u8; 6] = [0; 6];
-      match inst {
-          &Instruction::Ret => inst_slice[0] = 1u8,
-          &Instruction::Push(x1b, y2b) => {
-              inst_slice[0] = 2u8;
-              inst_slice[1] = x1b;
-              let y = y2b.to_le_bytes();
-              inst_slice[2] = y[0];
-              inst_slice[3] = y[1];
-          }
-          &Instruction::Pop => inst_slice[0] = 3u8,
-          &Instruction::Mov(x2b, y1b, z2b) => {
-              inst_slice[0] = 4u8;
-              let x = x2b.to_le_bytes();
-              inst_slice[1] = x[0];
-              inst_slice[2] = x[1];
-              inst_slice[3] = y1b;
-              let z = z2b.to_le_bytes();
-              inst_slice[4] = z[0];
-              inst_slice[5] = z[1];
-          }
-          &Instruction::Alloc(x1b, y2b) => {
-              inst_slice[0] = 5u8;
-              let y = y2b.to_le_bytes();
-              inst_slice[1] = y[0];
-              inst_slice[2] = y[1];
-          }
-          &Instruction::Freet => inst_slice[0] = 6u8,
-          &Instruction::Add => inst_slice[0] = 7u8,
-          &Instruction::Sub => inst_slice[0] = 8u8,
-          &Instruction::Mul => inst_slice[0] = 9u8,
-          &Instruction::Div => inst_slice[0] = 10u8,
-          &Instruction::And => inst_slice[0] = 11u8,
-          &Instruction::Or => inst_slice[0] = 13u8,
-          &Instruction::Xor => inst_slice[0] = 14u8,
-          &Instruction::Cmp => inst_slice[0] = 15u8,
-          &Instruction::Jmp(x4b) => {
-              inst_slice[0] = 16u8;
-              let x = x4b.to_le_bytes();
-              inst_slice[1] = x[0];
-              inst_slice[2] = x[1];
-              inst_slice[3] = x[2];
-              inst_slice[4] = x[3];
-          }
-          &Instruction::Je(x4b) => {
-              inst_slice[0] = 17u8;
-              let x = x4b.to_le_bytes();
-              inst_slice[1] = x[0];
-              inst_slice[2] = x[1];
-              inst_slice[3] = x[2];
-              inst_slice[4] = x[3];
-          }
-          &Instruction::Jl(x4b) => {
-              inst_slice[0] = 18u8;
-              let x = x4b.to_le_bytes();
-              inst_slice[1] = x[0];
-              inst_slice[2] = x[1];
-              inst_slice[3] = x[2];
-              inst_slice[4] = x[3];
-          }
-          &Instruction::Jle(x4b) => {
-              inst_slice[0] = 19u8;
-              let x = x4b.to_le_bytes();
-              inst_slice[1] = x[0];
-              inst_slice[2] = x[1];
-              inst_slice[3] = x[2];
-              inst_slice[4] = x[3];
-          }
-          &Instruction::Jg(x4b) => {
-              inst_slice[0] = 20u8;
-              let x = x4b.to_le_bytes();
-              inst_slice[1] = x[0];
-              inst_slice[2] = x[1];
-              inst_slice[3] = x[2];
-              inst_slice[4] = x[3];
-          }
-          &Instruction::Jge(x4b) => {
-              inst_slice[0] = 21u8;
-              let x = x4b.to_le_bytes();
-              inst_slice[1] = x[0];
-              inst_slice[2] = x[1];
-              inst_slice[3] = x[2];
-              inst_slice[4] = x[3];
-          }
-          &Instruction::Call(x4b) => {
-              inst_slice[0] = 22u8;
-              let x = x4b.to_le_bytes();
-              inst_slice[1] = x[0];
-              inst_slice[2] = x[1];
-              inst_slice[3] = x[2];
-              inst_slice[4] = x[3];
-          }
-          &Instruction::Fun(x4b) => {
-              inst_slice[0] = 23u8;
-              let x = x4b.to_le_bytes();
-              inst_slice[1] = x[0];
-              inst_slice[2] = x[1];
-              inst_slice[3] = x[2];
-              inst_slice[4] = x[3];
-          }
-      }
-      inst_slice
+pub fn assemble(inst: &NoLabelInst) -> [u8; 6] {
+    let mut inst_slice: [u8; 6] = [0; 6];
+    match inst {
+        &NoLabelInst::Ret => inst_slice[0] = 1u8,
+        &NoLabelInst::Push(x1b, y2b) => {
+            inst_slice[0] = 2u8;
+            inst_slice[1] = x1b;
+            let y = y2b.to_le_bytes();
+            inst_slice[2] = y[0];
+            inst_slice[3] = y[1];
+        }
+        &NoLabelInst::Pop => inst_slice[0] = 3u8,
+        &NoLabelInst::Mov(x2b, y1b, z2b) => {
+            inst_slice[0] = 4u8;
+            let x = x2b.to_le_bytes();
+            inst_slice[1] = x[0];
+            inst_slice[2] = x[1];
+            inst_slice[3] = y1b;
+            let z = z2b.to_le_bytes();
+            inst_slice[4] = z[0];
+            inst_slice[5] = z[1];
+        }
+        &NoLabelInst::Alloc(x1b, y2b) => {
+            inst_slice[0] = 5u8;
+            inst_slice[1] = x1b;
+            let y = y2b.to_le_bytes();
+            inst_slice[2] = y[0];
+            inst_slice[3] = y[1];
+        }
+        &NoLabelInst::Freet => inst_slice[0] = 6u8,
+        &NoLabelInst::Add => inst_slice[0] = 7u8,
+        &NoLabelInst::Sub => inst_slice[0] = 8u8,
+        &NoLabelInst::Mul => inst_slice[0] = 9u8,
+        &NoLabelInst::Div => inst_slice[0] = 10u8,
+        &NoLabelInst::And => inst_slice[0] = 11u8,
+        &NoLabelInst::Or => inst_slice[0] = 13u8,
+        &NoLabelInst::Xor => inst_slice[0] = 14u8,
+        &NoLabelInst::Cmp => inst_slice[0] = 15u8,
+        &NoLabelInst::Jmp(x4b) => {
+            inst_slice[0] = 16u8;
+            let x = x4b.to_le_bytes();
+            inst_slice[1] = x[0];
+            inst_slice[2] = x[1];
+            inst_slice[3] = x[2];
+            inst_slice[4] = x[3];
+        }
+        &NoLabelInst::Je(x4b) => {
+            inst_slice[0] = 17u8;
+            let x = x4b.to_le_bytes();
+            inst_slice[1] = x[0];
+            inst_slice[2] = x[1];
+            inst_slice[3] = x[2];
+            inst_slice[4] = x[3];
+        }
+        &NoLabelInst::Jl(x4b) => {
+            inst_slice[0] = 18u8;
+            let x = x4b.to_le_bytes();
+            inst_slice[1] = x[0];
+            inst_slice[2] = x[1];
+            inst_slice[3] = x[2];
+            inst_slice[4] = x[3];
+        }
+        &NoLabelInst::Jle(x4b) => {
+            inst_slice[0] = 19u8;
+            let x = x4b.to_le_bytes();
+            inst_slice[1] = x[0];
+            inst_slice[2] = x[1];
+            inst_slice[3] = x[2];
+            inst_slice[4] = x[3];
+        }
+        &NoLabelInst::Jg(x4b) => {
+            inst_slice[0] = 20u8;
+            let x = x4b.to_le_bytes();
+            inst_slice[1] = x[0];
+            inst_slice[2] = x[1];
+            inst_slice[3] = x[2];
+            inst_slice[4] = x[3];
+        }
+        &NoLabelInst::Jge(x4b) => {
+            inst_slice[0] = 21u8;
+            let x = x4b.to_le_bytes();
+            inst_slice[1] = x[0];
+            inst_slice[2] = x[1];
+            inst_slice[3] = x[2];
+            inst_slice[4] = x[3];
+        }
+        &NoLabelInst::Call(x4b) => {
+            inst_slice[0] = 22u8;
+            let x = x4b.to_le_bytes();
+            inst_slice[1] = x[0];
+            inst_slice[2] = x[1];
+            inst_slice[3] = x[2];
+            inst_slice[4] = x[3];
+        }
+        &NoLabelInst::Fun(x2b) => {
+            inst_slice[0] = 23u8;
+            let x = x2b.to_le_bytes();
+            inst_slice[1] = x[0];
+            inst_slice[2] = x[1];
+        }
+    }
+    inst_slice
 }
