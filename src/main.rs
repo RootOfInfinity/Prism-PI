963256f734e810f5fafec2228e64fa6b2ee5e066
William Campbell <numbersdev@protonmail.com>
2025-11-20 19:57:17 -0600

    cleanup to make it an awesome executable

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,17 @@ mod tester;
 
 fn main() -> Result<(), slint::PlatformError> {
     // println!("Hello, world!");
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        println!("Use with argument 'lang' or 'gui'");
-        println!("If using 'cargo run', the command would look like:");
-        println!("cargo run -- gui");
-    } else {
-        match args[1].as_str() {
-            "lang" => crate::lang::run_lang_test(args),
-            "gui" => crate::gui::run_gui_test(args)?,
-            x => println!("Invalid argument: {}", x),
-        }
-    }
-    Ok(())
+    // let args: Vec<String> = env::args().collect();
+    // if args.len() < 2 {
+    //     println!("Use with argument 'lang' or 'gui'");
+    //     println!("If using 'cargo run', the command would look like:");
+    //     println!("cargo run -- gui");
+    // } else {
+    //     match args[1].as_str() {
+    //         "lang" => crate::lang::run_lang_test(args),
+    //         "gui" => crate::gui::run_gui_test(args)?,
+    //         x => println!("Invalid argument: {}", x),
+    //     }
+    // }
+    crate::gui::run_gui_test(Vec::new())
 }
